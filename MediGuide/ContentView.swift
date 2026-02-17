8a692642fb8346a3215279567f201d7c6be7c9aa
Ashmita Appineni <4aappineni@gmail.com>
2026-02-17 11:45:19 -0600

    Initial Commit

---
+++ MediGuide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MediGuide
+//
+//  Created by Ashmita Appineni on 2/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MediGuide/MediGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediGuideApp.swift
+//  MediGuide
+//
+//  Created by Ashmita Appineni on 2/17/26.
+//
+
+import SwiftUI
+
+@main
+struct MediGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
